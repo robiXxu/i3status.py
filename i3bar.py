f1c017727ea1211c9c3f68a067246c5dc9dbe6f5
Schiriac Robert <schiriac.robert@gmail.com>
2021-05-08 03:41:56 +0300

    updated

--- i3bar.py
+++ i3bar.py
@@ -6,14 +6,16 @@ import socket
 import psutil
 import dbus
 
-mainColor="#2E3440"
+main_color="#2E3440"
+# in seconds
+update_rate = 10
 
 def separator(c1, c2):
     obj = {
         "full_text": "",
         "separator": "false",
         "separator_block_width": 0,
-        "border": mainColor,
+        "border": main_color,
         "border_left": 0,
         "border_right": 0,
         "border_top": 0,
@@ -24,7 +26,7 @@ def separator(c1, c2):
     return json.dumps(obj)
 
 
-def item(name, full_text, color, background, prevColor):
+def item(name, full_text, color, background, prev_color):
     obj = {
         "full_text": full_text,
         "name": name,
@@ -32,13 +34,13 @@ def item(name, full_text, color, background, prevColor):
         "background": background,
         "separator": "false",
         "separator_block_width": 0,
-        "border": mainColor,
+        "border": main_color,
         "border_left": 0,
         "border_right": 0,
         "border_top": 0,
         "border_bottom": 0,
     }
-    return ",".join([separator(background, prevColor), json.dumps(obj)])
+    return ",".join([separator(background, prev_color), json.dumps(obj)])
 
 def getSpotifyInfo():
     try:
@@ -70,45 +72,45 @@ if __name__ == '__main__':
                     full_text=" {} ".format(getSpotifyInfo()),
                     color="#444444",
                     background="#DBCB7E",
-                    prevColor=mainColor
+                    prev_color=main_color
                 ),
                 item(
                     name="id_ip_local",
                     full_text="  {} ".format(socket.gethostbyname(socket.gethostname())),
                     color="#333333",
                     background="#C49D58",
-                    prevColor="#DBCB7E"
+                    prev_color="#DBCB7E"
                 ),
                 item(
                     name="id_disk_usage",
                     full_text="  {}% ".format(psutil.disk_usage('/').percent),
                     color="#EEEEEE",
                     background="#3949AB",
-                    prevColor="#C49D58"
+                    prev_color="#C49D58"
                 ),
                 item(
                     name="id_memory",
                     full_text="  {}% ".format(psutil.virtual_memory()[2]),
                     color="#DDDDDD",
                     background="#B87238",
-                    prevColor="#3949AB"
+                    prev_color="#3949AB"
                 ),
                 item(
                     name="id_cpu_usage",
                     full_text="  {}% ".format(psutil.cpu_percent(interval=1)),
                     color="#FFFFFF",
                     background="#A7282E",
-                    prevColor="#B87238"
+                    prev_color="#B87238"
                 ),
                 item(
                     name='id_date',
                     full_text="  {} ".format(datetime.now().strftime("%H:%M %a %d-%B")),
                     color="#333333",
                     background="#80B3B1",
-                    prevColor="#A7282E",
+                    prev_color="#A7282E",
                 ),
-                separator(mainColor, "#80B3B1")
+                separator(main_color, "#80B3B1")
         ]
         print(",[{}]".format(",".join(items)))
-        time.sleep(5)
+        time.sleep(update_rate)
 
